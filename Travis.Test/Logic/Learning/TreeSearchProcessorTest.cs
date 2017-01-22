cd0d86a39a8727a6cfd3c4ffc17e753e72aa221e
TrolleY <trolleyyy@gmail.com>
2017-01-22 13:34:18 +0100

    deep check of tree structure

--- Travis.Test/Logic/Learning/TreeSearchProcessorTest.cs
+++ Travis.Test/Logic/Learning/TreeSearchProcessorTest.cs
@@ -13,18 +13,24 @@ namespace Travis.Test.Logic.Learning
     [TestClass]
     public class TreeSearchProcessorTest
     {
+        /// <summary>
+        /// Checks num visited of not fully expanded tree.
+        /// </summary>
         [TestMethod]
         public void NumVisitedTest()
         {
             var processor = new TreeSearchProcessor();
-            var iterations = 1000;
+            var iterations = 100;
             var tree = new TreeNode();
-            var game = new GreedyNumbers(2, new Dictionary<int, int>() { { 1, 5 }, { 2, 3 }, { 7, 1 } });
+            var game = new GreedyNumbers(2, new Dictionary<int, int>() { { 1, 1000 }, { 2, 3 }, { 5, 12 }, { 12000, 1 } });
             processor.Process(tree, game, iterations, MCTSActionSelector.Create(game.EnumerateActors()));
             Assert.AreEqual(iterations, tree.Quality.NumVisited);
-            AssertActionQualities(tree);
+            AssertTreeNumVisited(tree, iterations);
         }
 
+        /// <summary>
+        /// Checks structure of tree which is a path.
+        /// </summary>
         [TestMethod]
         public void NumVisitedTest_TreeIsAPath()
         {
@@ -34,17 +40,12 @@ namespace Travis.Test.Logic.Learning
             var game = new GreedyNumbers(2, new Dictionary<int, int>() { { 1, 3 } });
             processor.Process(tree, game, iterations, MCTSActionSelector.Create(game.EnumerateActors()));
             Assert.AreEqual(iterations, tree.Quality.NumVisited);
-            var testNode = tree;
-            int depth = -1;
-            while (testNode != null)
-            {
-                Assert.AreEqual(iterations - (depth < 0 ? 0 : depth), testNode.Quality.NumVisited, "Incorrect at depth {0}", depth);
-                testNode = testNode.Children.Values.SingleOrDefault();
-                depth++;
-            }
-            AssertActionQualities(tree);
+            AssertTreeNumVisited(tree, iterations);
         }
 
+        /// <summary>
+        /// Checks structure of tree which is initially fully expanded.
+        /// </summary>
         [TestMethod]
         public void NumVisitedTest_SingleIterationFullyExpands()
         {
@@ -54,42 +55,46 @@ namespace Travis.Test.Logic.Learning
             var game = new GreedyNumbers(2, new Dictionary<int, int>() { { 1, 1 } });
             processor.Process(tree, game, iterations, MCTSActionSelector.Create(game.EnumerateActors()));
             Assert.AreEqual(iterations, tree.Quality.NumVisited);
-            var testNode = tree;
-            int depth = -1;
-            while (testNode != null)
-            {
-                Assert.AreEqual(iterations - (depth < 0 ? 0 : depth), testNode.Quality.NumVisited, "Incorrect at depth {0}", depth);
-                testNode = testNode.Children.Values.SingleOrDefault();
-                depth++;
-            }
-            AssertActionQualities(tree);
+            AssertTreeNumVisited(tree, iterations);
+
             var newIterations = 999;
             processor.Process(tree, game, newIterations, MCTSActionSelector.Create(game.EnumerateActors()));
             Assert.AreEqual(iterations + newIterations, tree.Quality.NumVisited);
-            testNode = tree;
-            depth = -1;
-            while (testNode != null)
-            {
-                Assert.AreEqual(iterations + newIterations - (depth < 0 ? 0 : depth), testNode.Quality.NumVisited, "Incorrect at depth {0}", depth);
-                testNode = testNode.Children.Values.SingleOrDefault();
-                depth++;
-            }
-            AssertActionQualities(tree);
+            AssertTreeNumVisited(tree, iterations + newIterations);
         }
 
-        private void AssertActionQualities(TreeNode node)
+        private void AssertTreeNumVisited(TreeNode node, int iterations, int depth = 0)
         {
+            if (node.IsTerminal)
+            {
+                Assert.IsFalse(node.Children.Any());
+                Assert.IsFalse(node.Quality.ActorActionsQualities.Any());
+                return;
+            }
+
+            // Sum of children num visited is equal to parent node num visited decremented by one.
+            // The exception is for root node, because root node is initially not visited.
+            Assert.AreEqual(iterations - (depth == 0 ? 0 : 1), node.Children.Sum(ch => ch.Value.Quality.NumVisited), "Incorrect at depth {0}", depth);
+
             foreach (var actorActionsQuality in node.Quality.ActorActionsQualities.Values)
             {
+                // Action to appear in quality info, must be visited at least once.
                 Assert.IsFalse(actorActionsQuality.Values.Any(q => q.NumSelected == 0));
+
+                // Sum of num visited for actor actions must be equal to node's num visited.
                 Assert.AreEqual(node.Quality.NumVisited, actorActionsQuality.Values.Sum(q => q.NumSelected));
             }
+
+            // Assert child node.
             foreach (var childNode in node.Children.Values)
             {
-                AssertActionQualities(childNode);
+                AssertTreeNumVisited(childNode, childNode.Quality.NumVisited, depth + 1);
             }
         }
 
+        /// <summary>
+        /// Checks if top action has best quality and is is mostly selected.
+        /// </summary>
         [TestMethod]
         public void QualityTest_BestActionWithGreatesValue()
         {
