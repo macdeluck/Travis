16e754d4c8cb7161eb87119d9e7767d9a0d6f0fc
TrolleY <trolleyyy@gmail.com>
2017-02-10 18:09:28 +0100

    learn command

--- Travis.Console/LearnProgram.cs
+++ Travis.Console/LearnProgram.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Travis.Console.Commandline;
+using Travis.Logic.Learning;
 using Travis.Logic.Learning.Model;
+using Travis.Logic.MCTS;
+using Travis.Logic.Model;
 
 namespace Travis.Console
 {
@@ -12,6 +16,10 @@ namespace Travis.Console
     {
         private IBudgetProvider budgetProvider;
 
+        private IGame game;
+
+        private IDictionary<int, ActionSelector> actionSelectors;
+
         /// <summary>
         /// Runs learn program.
         /// </summary>
@@ -21,6 +29,20 @@ namespace Travis.Console
             if (!options.BudgetArgumentList.Any())
                 budgetProvider = TravisInit.Current.GetObject<IBudgetProvider>(options.BudgetProviderName);
             else budgetProvider = TravisInit.Current.GetObject<IBudgetProvider>(options.BudgetProviderName, options.BudgetArgumentList);
+            game = TravisInit.Current.GetObject<IGame>(options.GameName);
+            if (options.SelectorNames.Any())
+            {
+                var actorIds = game.EnumerateActors().ToList();
+                if (options.SelectorNames.Count != actorIds.Count)
+                    throw new Exception();
+                actionSelectors = options.SelectorNames.Zip(actorIds, (n, i) => new { SelectorName = n, ActorId = i })
+                    .ToDictionary(sel => sel.ActorId, sel => TravisInit.Current.GetObject<ActionSelector>(sel.SelectorName));
+            }
+            else actionSelectors = MCTSActionSelector.Create(game.EnumerateActors());
+
+            var tree = new TreeNode();
+            var processor = new TreeSearchProcessor();
+            processor.Process(tree, game, budgetProvider, actionSelectors);
         }
     }
 }
